d275ba1fd86c4ab0c946990d5ea0e7033cb6d2fa
Duncan Hunter <duncan@duncanhunter.com.au>
2017-01-27 12:55:39 -0800

    fix - simplfy files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  `,
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor() { }
 }
